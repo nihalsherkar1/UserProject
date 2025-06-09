b9763316320245234c0dfe619d5f7946c9120818
Nihalahmad Aslam Sherkar <nihalsherkar1@gmail.com>
2025-06-09 09:10:22 +0530

    Update UserRepository.java

--- src/main/java/com/user/repository/UserRepository.java
+++ src/main/java/com/user/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.user.repository;
 
 import com.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -12,6 +13,7 @@ import java.util.List;
 public interface UserRepository  extends JpaRepository<User, Integer> {
 
     @Procedure(name = "User.getUsersByDepartment")
+    @Query("SELECT d FROM User as d WHERE d.department = :dept ")
     List<User>getUserByDepartment(@Param("dept") String department);
 
 
